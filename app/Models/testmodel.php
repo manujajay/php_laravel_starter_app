b58c38f74105d371224296f5213d9c84e74a57d1
manujajay <manujajayawardanais@gmail.com>
2024-01-17 22:30:13 +0000

    adding all files

---
+++ app/Models/testmodel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class testmodel extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/hello.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Page</title>
+</head>
+<body>
+    <h1>Hello from Laravel</h1>
+</body>
+</html>
